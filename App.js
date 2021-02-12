f64d48212e59feb909d9052c41c9c621a8aa4056
Eleni Fatsea <efatsea@gmail.com>
2021-02-12 16:24:04 +0200

    Add tab navigation in second screen

--- App.js
+++ App.js
@@ -4,9 +4,12 @@ import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const HomeScreen = ({navigation}) =>{
   return(
@@ -15,20 +18,57 @@ const HomeScreen = ({navigation}) =>{
       <Text h3>We will show you all the nice interesting places you can go!</Text>
       <Button
         title='Let me show you'
-        onPress = {()=>navigation.navigate('List')}
+        onPress = {()=>navigation.navigate('POI')}
        />
       <StatusBar style="auto" />
     </View>
     )
 }
 
+const List=()=> {
+  return (
+    <View style={styles.tab}>
+      <Text>List</Text>
+    </View>
+  );
+}
+
+const Maps=()=>{
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Map</Text>
+    </View>
+  );
+}
+
 const Poi = ()=>{
   return(
-    <View style={styles.container}>
-      <Text h1>List of POIs</Text>
-    </View>
+      <Tab.Navigator
+        screenOptions={({route})=>({
+          tabBarIcon:()=>{
+            let iconName;
+            if (route.name==='List'){
+              iconName= "list-outline"
+            }
+            else iconName = 'earth-outline'
+
+            return <Ionicons name={iconName} size={30}/>
+          }
+        })}
+      >
+        <Tab.Screen
+          name='List'
+          component={List}
+        />
+        <Tab.Screen
+          name='Map'
+          component={Maps}
+        /> 
+      </Tab.Navigator>
+   
   )
 }
+
 
 
 export default function App() {
@@ -42,7 +82,7 @@ export default function App() {
               options={{title: 'Welcome'}}
             />
             <Stack.Screen
-              name='List'
+              name='POI'
               component={Poi}
             />
           </Stack.Navigator>
@@ -58,4 +98,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tab:{
+    flex:1,
+    backgroundColor: '#78A2CC',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+
 });
