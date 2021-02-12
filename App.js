d97c27cad3bf4f6321cf6ba9225a58ea66e524e1
Eleni Fatsea <efatsea@gmail.com>
2021-02-12 15:11:17 +0200

    Add a second screen. The home screen has a button to navigate in the List Screen

--- App.js
+++ App.js
@@ -1,14 +1,54 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
+
+const Stack = createStackNavigator();
+
+const HomeScreen = ({navigation}) =>{
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text h1>Welcome!</Text>
+      <Text h3>We will show you all the nice interesting places you can go!</Text>
+      <Button
+        title='Let me show you'
+        onPress = {()=>navigation.navigate('List')}
+       />
       <StatusBar style="auto" />
     </View>
-  );
+    )
+}
+
+const Poi = ()=>{
+  return(
+    <View style={styles.container}>
+      <Text h1>List of POIs</Text>
+    </View>
+  )
+}
+
+
+export default function App() {
+
+    return (
+      <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name='Home'
+              component={HomeScreen}
+              options={{title: 'Welcome'}}
+            />
+            <Stack.Screen
+              name='List'
+              component={Poi}
+            />
+          </Stack.Navigator>
+      </NavigationContainer>
+      
+    );
 }
 
 const styles = StyleSheet.create({
